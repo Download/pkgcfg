c9e2ae00e6657fd8b7902833e732027d579b2056
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-01-03 11:56:17 +0100

    0.1.0

---
+++ pkgcfg.js
@@ -0,0 +1,17 @@
+﻿var
+fs = require('fs'),
+pkg = JSON.parse(fs.readFileSync('package.json')),
+objectPath = require('object-path'),
+path = require('path');
+
+function pkgcfg(path) {
+	var root = path ? objectPath.get(pkg, path) : pkg;
+	resolved = resolve(root);
+	return resolved;
+}
+
+function resolve(root, node, resolveTree) {
+	return root;
+}
+
+module.exports = pkgcfg;
---
+++ pkgcfg.spec.js
@@ -0,0 +1,26 @@
+﻿var pkgcfg = require('../pkgcfg');
+var expect = require('chai').expect;
+
+describe('pkgcfg', function(){
+	it('is a function', function(){
+		expect(pkgcfg).to.be.a('function');
+	});
+	it('returns an object', function(){
+		expect(pkgcfg()).to.be.an('object');
+	});
+	it('accepts a string or array of strings as parameter', function(){
+		expect(pkgcfg('keywords')).to.be.an('array');
+		expect(pkgcfg(['keywords'])).to.be.an('array');
+	});
+	it('returns an object representation of package.json when given no arguments', function(){
+		var pkg = pkgcfg();
+		expect(pkg).to.have.a.property('name');
+		expect(pkg.name).to.equal('pkgcfg');
+		expect(pkg).to.have.a.property('version');
+		expect(pkg).to.have.a.property('description');
+	});
+	it('returns a subtree from package.json when given an argument', function(){
+		var keywords = pkgcfg('keywords');
+		expect(keywords).to.be.an('array');
+	});
+});
