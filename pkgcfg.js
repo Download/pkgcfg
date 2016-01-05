51ea0cedf59bcac7c214beea0162df2c065c62a6
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-01-05 15:21:48 +0100

    0.2.0

--- pkgcfg.js
+++ pkgcfg.js
@@ -1,17 +1,175 @@
-﻿var
-fs = require('fs'),
-pkg = JSON.parse(fs.readFileSync('package.json')),
-objectPath = require('object-path'),
-path = require('path');
+﻿var fs = require('fs');
+var log = require('picolog');
+var objectPath = require("object-path");
 
-function pkgcfg(path) {
-	var root = path ? objectPath.get(pkg, path) : pkg;
-	resolved = resolve(root);
-	return resolved;
+var global = typeof window == 'object' ? window : (typeof global == 'object' ? global : this);
+
+function pkgcfg(pkg) {
+	if (! pkg) {pkg = '../../package.json';}
+	if (typeof pkg == 'string') {pkg = JSON.parse(fs.readFileSync(pkg));}
+	var root = pkg;
+	processed = process(pkg, root);
+	return processed;
 }
 
-function resolve(root, node, resolveTree) {
-	return root;
-}
+var registeredTransforms = {};
+pkgcfg.registry = {
+	register: register,
+	unregister: unregister,
+	getTransform: getTransform,
+	getTransformTags: getTransformTags
+};
+
+pkgcfg.registry.register('pkg', function(pkg, node, arg) {
+	var result;
+	if (arg) {
+		result = objectPath.get(pkg, arg);
+	}
+	if (result === undefined) {return '{pkg' + arg + '}';}
+	return result;
+});
 
 module.exports = pkgcfg;
+
+function register(tag, func) {
+	log.assert(tag, 'Parameter `tag` is required when registering a package transform: ', tag);
+	log.assert(typeof tag == 'string', 'Parameter `tag` must be a string: ', tag);
+	log.assert(!registeredTransforms[tag], 'A package transform with that tag is already registered: ', registeredTransforms[tag]);
+	log.assert(['toString', 'valueOf', 'toJSON'].indexOf(tag) === -1, '`%s` is a reserved word and may not be used as a tag for a package transform.', tag);
+	var usedReservedChars = ['{','}','[',']','(',')'].filter(
+		function(x){
+			return tag.indexOf(x) !== -1;
+		}
+	);
+	log.assert(usedReservedChars.length === 0, 'Parameter `tag` contains reserved character(s): ', usedReservedChars);
+	log.assert(func, 'Parameter `func` is required when registering a package transform: ', func);
+	log.assert(typeof func == 'function', 'Parameter `func` must be a function when registering a package transform: ', func);
+	registeredTransforms[tag] = func;
+}
+
+function unregister(tag, func) {
+	log.assert(registeredTransforms[tag] && registeredTransforms[tag] === func, 'Unable to unregister the package transform. The supplied function is not currently registered with the supplied tag %s.', tag);
+	log.assert(['toString', 'valueOf', 'toJSON'].indexOf(tag) === -1, '`%s` is a reserved word and may not be used as a tag for a package transform.', tag);
+	delete registeredTransforms[tag];
+}
+
+function getTransform(tag) {
+	return registeredTransforms[tag];
+}
+
+function getTransformTags() {
+	return Object.keys(registeredTransforms);
+}
+
+function process(pkg, node) {
+	// array
+	if (node instanceof Array) {return processArray(pkg, node);}
+	// non-null object
+	if (node && (typeof node == 'object')) {return processObject(pkg, node);}
+	// primitives, null, undefined
+	if (typeof node !== 'string') {return node;} // primitive, no need to clone
+	// strings (that might contain transform tags)
+	return processString(pkg, node);
+}
+
+function processArray(pkg, node) {
+	var result = [];
+	for (var i=0; i<node.length; i++) {
+		result.push(process(pkg, node[i]));
+	}
+	return result;
+}
+
+function processObject(pkg, node) {
+	var result = {};
+	var keys = Object.keys(node);
+	for (var i=0; i<keys.length; i++) {
+		result[keys[i]] = process(pkg, node[keys[i]]);
+	}
+	return result;
+}
+
+function processString(pkg, node) {
+	var next, input = node, result = [], complex=false;
+	while ((next = nextTag(input)) !== null) {
+		var before = input.substring(0, next.idx);
+		if (before) {result.push(before);}
+		var remaining = input.substring(next.idx + next.tag.length + 1);
+		var body = tagBody(remaining);
+		if (body) {
+			var payload = body.process ? processString(pkg, body.arg) : body.arg;
+			var transformed = transform(pkg, node, next.tag, payload);
+			var isStr = typeof transformed == 'string';
+			if (!isStr) {complex = true;}
+			if (!isStr || transformed.indexOf('{' + next.tag) !== 0) { // prevent recursion
+				// 2nd pass
+				transformed = process(pkg, transformed);
+			}
+			result.push(transformed);
+			input = remaining.substring(body.end + 1);
+		}
+		else { // invalid, return original text
+			result.push('{' + next.tag + remaining);
+		}
+	}
+	if (input) {result.push(input);}
+	return complex ? (result.length === 1 ? result[0] : result) : result.join('');
+}
+
+function nextTag(tokenstream) {
+	var tags = Object.keys(registeredTransforms);
+	var NOTFOUND = 999999999;
+	var next = {tag:null, idx:NOTFOUND};
+	for (var i=0; i<tags.length; i++) {
+		var idx = tokenstream.indexOf('{' + tags[i]);
+		if ((idx !== -1) && (idx < next.idx)) {
+			next.idx = idx;
+			next.tag = tags[i];
+		}
+	}
+	return next.tag ? next : null;
+}
+
+function tagBody(tokenstream) {
+	// loop through the string, parsing it as we go through it
+	// return the fully resolved tagBody, or null if we encounter illegal state
+	var result = {process:null, arg:null, end:-1};
+	var inString=false;
+	var esc = false;
+	var open=0;
+	var whitespace = /\s/;
+	for (var i=0; i<tokenstream.length; i++) {
+		var token = tokenstream[i];
+		if (result.process === null) {
+			if (token.match(whitespace)) {continue;}
+			if (token === '}') {result.end = i; return result;} // 0
+			result.process = inString = token === '\'';
+			result.arg = inString ? '' : token;
+			continue;
+		}
+		if (inString) {
+			if (token === '\'') {
+				inString = !inString;
+				esc = true;
+				continue;
+			}
+		}
+		if (token === '}') {result.end = i; return result;} // 1 or 2
+		result.arg += token;
+		if (esc && (token === '\'')) {inString = true; continue;}
+	}
+	return null;
+}
+
+function transform(pkg, node, tag, arg) {
+	try {
+		var result = registeredTransforms[tag](pkg, node, arg);
+		log.log('Package transform: %s(%s) => ', tag, arg, result);
+		return registeredTransforms[tag](pkg, node, arg);
+	}
+	catch(error) {
+		log.error('Error applying package transform `%s`: ', tag, error);
+		return node;
+	}
+}
+
--- pkgcfg.spec.js
+++ pkgcfg.spec.js
@@ -1,26 +1,100 @@
-﻿var pkgcfg = require('../pkgcfg');
+﻿var log = require('picolog');
+var pkgcfg = require('./pkgcfg');
 var expect = require('chai').expect;
 
 describe('pkgcfg', function(){
 	it('is a function', function(){
 		expect(pkgcfg).to.be.a('function');
 	});
-	it('returns an object', function(){
-		expect(pkgcfg()).to.be.an('object');
+	it('has a property `registry`', function(){
+		expect(pkgcfg).to.have.a.property('registry');
 	});
-	it('accepts a string or array of strings as parameter', function(){
-		expect(pkgcfg('keywords')).to.be.an('array');
-		expect(pkgcfg(['keywords'])).to.be.an('array');
+
+	describe('registry', function(){
+		it('is an object', function(){
+			expect(pkgcfg.registry).to.be.an('object');
+		});
+		it('has properties `register`, `unregister`, `getTransform` and `getTransformTags`', function(){
+			expect(pkgcfg.registry).to.have.a.property('register');
+			expect(pkgcfg.registry).to.have.a.property('unregister');
+			expect(pkgcfg.registry).to.have.a.property('getTransform');
+			expect(pkgcfg.registry).to.have.a.property('getTransformTags');
+		});
+
+		describe('register', function(){
+			it('is a function', function(){
+				expect(pkgcfg.registry.register).to.be.a('function');
+			});
+			it('can be used to register a new transform', function(){
+				expect(pkgcfg.registry.getTransformTags().length).to.equal(1);
+
+				pkgcfg.registry.register('hello', function(pkg, node, text) {
+					log.log('executing hello transform');
+					return 'Hello' + (text ? ', ' + text : '') + '!'
+				});
+
+				expect(pkgcfg.registry.getTransformTags().length).to.equal(2);
+				expect(pkgcfg.registry.getTransformTags().indexOf('hello')).not.to.equal(-1);
+
+				var pkg = pkgcfg('package.json');
+				expect(pkg.test.register).to.equal('Hello!');
+
+			});
+		});
+
+		describe('register', function(){
+			it('is a function', function(){
+				expect(pkgcfg.registry.register).to.be.a('function');
+			});
+		});
+
+		it('contains a built-in transform `pkg`', function(){
+			expect(pkgcfg.registry.getTransformTags().indexOf('pkg')).not.to.equal(-1);
+		});
+
+
 	});
-	it('returns an object representation of package.json when given no arguments', function(){
-		var pkg = pkgcfg();
-		expect(pkg).to.have.a.property('name');
-		expect(pkg.name).to.equal('pkgcfg');
-		expect(pkg).to.have.a.property('version');
-		expect(pkg).to.have.a.property('description');
-	});
-	it('returns a subtree from package.json when given an argument', function(){
-		var keywords = pkgcfg('keywords');
-		expect(keywords).to.be.an('array');
+
+	describe('when called', function(){
+		var pkg = pkgcfg('package.json');
+		it('returns an object representation of package.json when given a valid path', function(){
+			expect(pkg).to.have.a.property('name');
+			expect(pkg.name).to.equal('pkgcfg');
+			expect(pkg).to.have.a.property('version');
+			expect(pkg).to.have.a.property('description');
+		});
+		it('resolves `pkg` references to primitive values', function(){
+			log.info('{pkg name} => \'pkgcfg\'');
+			expect(pkg.test.primitive).to.equal(pkg.name);
+		});
+		it('resolves `pkg` references to primitive values on paths with multiple entries', function(){
+			log.info('{pkg repository.type} => \'git\'');
+			expect(pkg.test.primitive).to.equal(pkg.name);
+		});
+		it('resolves `pkg` references to objects', function(){
+			log.info('{pkg repository} => {type:\'git\', ..}');
+			expect(pkg.test.object).to.be.an('object');
+			expect(pkg.test.object).to.have.a.property('type');
+			expect(pkg.test.object.type).to.equal('git');
+		});
+		it('resolves `pkg` references to objects on paths with multiple entries', function(){
+			log.info('{pkg test.data} => {array:[\'one config to ..\', ..], ..}');
+			expect(pkg.test['nested-object']).to.be.an('object');
+			expect(pkg.test['nested-object']).to.have.a.property('array');
+			expect(pkg.test['nested-object'].array).to.be.an('array');
+			expect(pkg.test['nested-object'].array[2]).to.equal('pkgcfg');
+		});
+		it('resolves `pkg` references to arrays', function(){
+			log.info('{pkg keywords} => [\'package.json\', ...]');
+			expect(pkg.test.array).to.be.an('array');
+			expect(pkg.test.array).to.have.a.property('0');
+			expect(pkg.test.array[0]).to.equal('package.json');
+		});
+		it('resolves `pkg` references to arrays on paths with multiple entries', function(){
+			log.info('{pkg test.data.array} => [\'one config to ..\', ...]');
+			expect(pkg.test['nested-array']).to.be.an('array');
+			expect(pkg.test['nested-array']).to.have.a.property('2');
+			expect(pkg.test['nested-array'][2]).to.equal('pkgcfg');
+		});
 	});
 });
